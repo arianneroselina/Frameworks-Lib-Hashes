baaddab62579c5f60a91929d088e566d6ceafe0f
arianneroselina <arianne.prananto@cosateq.com>
2022-11-17 19:15:57 +0100

    finish hashing flutter and react native

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def hash_flutter(version, path):
         try:
             output = subprocess.check_output(one_file_command(path + folder + '\\' + flutter_file)).decode("utf-8")
 
-            start_index = output.find(flutter_file + ':') + len(flutter_file) + 1
+            start_index = output.find('so:') + 3
             end_index = output.find('CertUtil', start_index)
             output_hash = output[start_index+1: end_index].strip()
 
@@ -56,26 +56,19 @@ def hash_flutter(version, path):
 
 # get the hash of libreact*.so file from arm64_v8a, armeabi_v7a, x86, and x86_64 folders, then write them in the
 # corresponding .csv files.
-def hash_react_native(version, path):
-    path = add_slash_to_path(path)
+def hash_react_native(version, react_native_files):
+    for filepath in react_native_files:
+        splitted = str(filepath).split('\\')
+        filename = splitted[len(splitted)-1]
 
-    for folder in react_native_folders:
-        csv_file = f'files/react_native/{folder}.csv'
-        try:
-            print(multiple_files_command(path + folder + '\\', react_native_file))
-            output = subprocess.check_output(multiple_files_command(path + folder + '\\', react_native_file)).decode("utf-8")
+        for folder in react_native_folders:
+            csv_file = f'files/react_native/{folder}.csv'
+            try:
+                output = subprocess.check_output(one_file_command(filepath)).decode("utf-8")
 
-            # TODO : not yet edited
-            for output_row in output:
-                print("output_row", output_row)
-                start_filename_index = output_row.find('libreact')
-                end_filename_index = output_row.find('.so:') + 3
-                filename = output_row[start_filename_index+1: end_filename_index].strip()
-                print("filename", filename)
-
-                start_index = output_row.find('.so:') + 4
-                end_index = output_row.find('CertUtil', start_index)
-                output_hash = output_row[start_index+1: end_index].strip()
+                start_index = output.find('so:') + 3
+                end_index = output.find('CertUtil', start_index)
+                output_hash = output[start_index + 1: end_index].strip()
 
                 finish = False
                 versions_list = []
@@ -90,7 +83,7 @@ def hash_react_native(version, path):
                                     row[i] = output_hash
                                     change = False
                                     finish = True
-                                if row[i] == version:
+                                if row[i] == version and row[i+1] == filename:
                                     change = True
                             if len(row) != 0:
                                 versions_list.append(row)
@@ -101,10 +94,10 @@ def hash_react_native(version, path):
 
                 if not finish:
                     with open(csv_file, 'a') as csv_append:
-                        csv_append.write(version + ',' + output_hash)
+                        csv_append.write(f'{version},{filename},{output_hash}')
 
-        except subprocess.CalledProcessError as e:
-            raise RuntimeError("Command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
+            except subprocess.CalledProcessError as e:
+                    raise RuntimeError("Command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
 
 
 def one_file_command(filename):
@@ -154,4 +147,4 @@ if __name__ == '__main__':
         hash_flutter(sys.argv[1], sys.argv[2])
 
     if glob.glob(path + react_native_folders[0] + '\\' + react_native_file):
-        hash_react_native(sys.argv[1], sys.argv[2])
+        hash_react_native(sys.argv[1], glob.glob(path + react_native_folders[0] + '\\' + react_native_file))
